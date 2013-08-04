b001f22816e8ec937c63d952a90090e10b8f5586
Julian Ceipek <julian.ceipek@gmail.com>
2013-08-04 10:48:00 -0400

    Added ability to randomly generate connections; grew trigger = mass

--- vrjam/Assets/Scripts/Node.cs
+++ vrjam/Assets/Scripts/Node.cs
@@ -7,12 +7,34 @@ public class Node : MonoBehaviour {
     // Note: Will be made private
     public List<Connection> m_connections;
 
+
+    void Awake () {
+        if (m_connections != null) m_connections = new List<Connection>();
+    }
+
+    void Update () {
+        SphereCollider collider = gameObject.GetComponent<SphereCollider>();
+        Mass mass_component = gameObject.GetComponent<Mass>();
+        collider.radius = mass_component.Get() * RulesManager.g.m_MASS_TO_SIZE_RATIO / 2.0f;
+    }
+
     public void AddConnection (Connection connection) {
         m_connections.Add(connection);
     }
 
     public void RemoveConnection (Connection connection) {
         m_connections.Remove(connection);
+    }
+
+    public bool HasConnection (Connection connection) {
+        return m_connections.Contains(connection);
+    }
+
+    public bool DoesShareConnectionWith (Node other_node) {
+        foreach (Connection connection in m_connections) {
+            if (other_node.HasConnection(connection)) return true;
+        }
+        return false;
     }
 
     public void GainMass (float mass) {
@@ -33,7 +55,4 @@ public class Node : MonoBehaviour {
         }
     }
 
-    void Awake () {
-        if (m_connections != null) m_connections = new List<Connection>();
-    }
 }
--- vrjam/Assets/Scripts/RandomSphereGenerator.cs
+++ vrjam/Assets/Scripts/RandomSphereGenerator.cs
@@ -1,28 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RandomSphereGenerator : MonoBehaviour {
 
     public int m_node_count;
     public float m_sphere_radius;
+    public float m_connection_count;
 
     // Use this for initialization
     void Start () {
         Generate();
     }
 
-    private void GenerateNodeAt (Vector3 pos) {
+    private Node GenerateNodeAt (Vector3 pos) {
         GameObject node = (GameObject)Instantiate(Resources.Load("Node"));
         node.transform.position = pos;
         Mass mass_component = node.GetComponent<Mass>();
         mass_component.Initialize(Random.value * 2.0f + 0.5f);
+        return node.GetComponent<Node>();
     }
 
     private void Generate () {
+        List<Node> node_list = new List<Node>();
         for (int index = 0; index < m_node_count; index++) {
             //Vector3 random_vector = Random.onUnitSphere * m_sphere_radius;
             Vector3 random_vector = Random.insideUnitSphere * m_sphere_radius;
-            GenerateNodeAt(random_vector);
+            Node node = GenerateNodeAt(random_vector);
+            node_list.Add(node);
+        }
+
+        int max_connection_count = m_node_count*(m_node_count - 1) / 2;
+        if (m_connection_count > max_connection_count) {
+            m_connection_count = max_connection_count; // So we don't have an infinite loop!
+        }
+        bool success = false;
+        for (int index = 0; index < m_connection_count; index++) {
+            do {
+                int first = index % m_node_count;
+                int second = Random.Range(0, node_list.Count);
+                Node first_node = node_list[first];
+                Node second_node = node_list[second];
+                success = (first != second && !first_node.DoesShareConnectionWith(second_node));
+                if (success) {
+                    GameObject connection_object = (GameObject)Instantiate(Resources.Load("Connection"));
+                    Connection connection = connection_object.GetComponent<Connection>();
+                    connection.InitializeWithStartNode(first_node);
+                    connection.TryToFinishConnectionWithEndNode(second_node); // TODO: Change once this doesn't work anymore by giving the connection extra internal mass
+                }
+            } while (!success);
         }
     }
 }

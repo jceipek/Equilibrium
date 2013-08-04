6016d82a0b2b88c57355ea583028926464a14511
Julian Ceipek <julian.ceipek@gmail.com>
2013-08-04 11:25:41 -0400

    Made line visualization vary based on width

--- vrjam/Assets/Scripts/ConnectionVisualizationController.cs
+++ vrjam/Assets/Scripts/ConnectionVisualizationController.cs
@@ -14,10 +14,16 @@ public class ConnectionVisualizationController : MonoBehaviour {
         if (start != null) {
             LineRenderer renderer = gameObject.GetComponent<LineRenderer>();
             renderer.SetPosition(0, start.gameObject.transform.position);
+            Mass start_mass = start.GetComponent<Mass>();
+            float start_width = start_mass.Get() * RulesManager.g.m_MASS_TO_SIZE_RATIO / 2.0f;
             if (end != null) {
+                Mass end_mass = end.GetComponent<Mass>();
+                float end_width = start_mass.Get() * RulesManager.g.m_MASS_TO_SIZE_RATIO / 2.0f;
                 renderer.SetPosition(1, end.gameObject.transform.position);
+                renderer.SetWidth(start_width, end_width);
             } else {
                 renderer.SetPosition(1, end_point);
+                renderer.SetWidth(start_width, start_width);
             }
         }
     }

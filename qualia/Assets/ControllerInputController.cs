2dfe420e763bb076b59398be2612bf8d53caf01e
Julian Ceipek <julian.ceipek@gmail.com>
2013-08-19 23:54:35 -0400

    Made it possible to run the game without the rift.

---
+++ qualia/Assets/ControllerInputController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerInputController : MonoBehaviour {
+
+	public float m_RotationSpeed;
+	public bool m_InvertY;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float inverter = -1.0f;
+		if (m_InvertY)
+		{
+			inverter *= 1.0f;
+		}
+
+		float xInput = Input.GetAxis("Horizontal") * m_RotationSpeed;
+		float yInput = Input.GetAxis("Vertical") * m_RotationSpeed * inverter;
+		gameObject.transform.Rotate(xInput * Vector3.up * Time.deltaTime);
+		gameObject.transform.Rotate(yInput * Vector3.right * Time.deltaTime);
+	}
+}

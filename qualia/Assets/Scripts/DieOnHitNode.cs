c75ec15c9589f347f3dd3bdb1b4429ce339a8760
Julian Ceipek <julian.ceipek@gmail.com>
2013-08-22 00:47:57 -0400

    Added initial iteration of start+death

--- qualia/Assets/Scripts/DieOnHitNode.cs
+++ qualia/Assets/Scripts/DieOnHitNode.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class DieOnHitNode : MonoBehaviour
 {
+
+    public string m_DeathSceneName;
+
     void ReachedNode (Node node)
     {
         Infected infection = node.GetComponent<Infected>();
         if (infection && infection.enabled)
         {
+            Debug.Log(infection.enabled);
             Debug.Log("DIE!");
+            Destroy(gameObject);
+            Application.LoadLevel(m_DeathSceneName);
         }
     }
 }

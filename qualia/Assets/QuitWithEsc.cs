9243cd019f7778593053d4613523d6cf4e442936
Julian Ceipek <julian.ceipek@gmail.com>
2013-08-26 02:45:18 -0400

    Esc to quit

---
+++ qualia/Assets/QuitWithEsc.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitWithEsc : MonoBehaviour {
+	void Update ()
+	{
+		if (Input.GetKey("escape"))
+		{
+			Debug.Log("QUIT!");
+			Application.Quit();
+		}
+	}
+}
